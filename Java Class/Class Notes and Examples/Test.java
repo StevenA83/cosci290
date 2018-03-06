9106edb17df7b83bd8009a88807ade9011f27dc0
steven.a.lemos83 <steven.a.lemos83@gmail.com>
2018-03-06 18:43:27 -0500

    Lab6

---
+++ Java Class/Class Notes and Examples/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+
+  public static void main(String() args) {
+
+     java.util.Scanner input = new java.util.Scanner(System.out);
+
+     double x = input.nextInt();
+
+     double y = input.nextDouble();
+
+     double z = input.nextDouble();
+
+     system.out.println((x < y && y < z) ? "sorted" : "not sorted"); 
+
+  }
+
+}
