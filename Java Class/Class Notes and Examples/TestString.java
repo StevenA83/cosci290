719be3ca0c8fb1b3c2a21a8b95fe4346aa1ca8e1
steven.a.lemos83 <steven.a.lemos83@gmail.com>
2018-03-27 16:11:44 -0400

    String notes

---
+++ Java Class/Class Notes and Examples/TestString.java
@@ -0,0 +1,44 @@
+/*
+    Steven Lemos
+    Co Sci 290
+    
+    String notes
+    
+*/
+
+public class TestString{
+
+  public static void main(String[]args){
+    
+    /*
+      Methods in the Character Class
+      
+      isDigit(ch) ==> returns true if the specified character is a digit
+      isLetter(ch) ==> returns true if the specified character is a letter
+      isLetterOrDigit(ch) ==> returns ture if the specified character is a letter or digit
+      isLowerCase(ch) ==> returns true if the specified character is a lowercase letter
+      isUpperCase(ch) ==> returns true if the specified character is a uppercase letter
+      toLowerCase(ch) ==> returns the lowercase of the specified character
+      toUpperCase(ch) ==> returns the uppercase of the specified character
+      
+      Methods for String Objects
+      String.lenth() ==> returns the number of characters in this string
+      String.charAt(index) ==> returns the character at the specified index of this string
+      String.concat(s1) ==> returns a new string that concatenates this string with string s1
+      
+      Comparison Methods for String Objects
+      String.equals(s1) ==> returns true if this string is equal to string s1, it is case sensitive
+      String.equalsIgnoreCase(s1) ==> returns true if this string is equal to string s1 and is not case sensitive
+      String.startsWith(prefix) ==> returns true if this string starts with the specified prefix
+      Sting.endsWith(suffix) ==> returns true if this string ends with the specified suffix
+      String.contains(s1) ==> returns true if s1 is a substring in this stirng
+      
+      Methods for obtaining substrings
+      String.substring(beginIndex) ==> returns this string's substring that begins with the character at the specified beginIndex number and extends
+        to the end of the string
+      String.substring(beginIndex,endIndex) ==> returns this string's substring that begins wiht the character at the specified beginIndex number and extends 
+        to the character at the endIndex number - 1
+    */
+  }
+}
+     
