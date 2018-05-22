2e0ae98cb3337560b07bd679a1f285adb7524a09
steven.a.lemos83 <steven.a.lemos83@gmail.com>
2018-05-22 18:01:33 -0400

    Lab 21

--- Java Class/Homework Assignments/TicTacToe.java
+++ Java Class/Homework Assignments/TicTacToe.java
@@ -90,16 +90,59 @@ public class TicTacToe{
           changeXO();
         }//end of else statement
       }//end of if((col)) statement
+      else 
+        System.out.println("Sorry, you've entered and invalid option.  Please chooose again player " + xo + "\n");
+        changeXO();
     }//end of if((row)) statement
+    else 
+      System.out.println("Sorry, you've entered and invalid option.  Please chooose again player " + xo + "\n");
+      changeXO();
   }//end of move method
   
   /*
     Returns true if there there is a winner or a draw to end the game
   */
   public static boolean checkWinner(){
-    
-
-    
+    int counter = 0;
+    for(int i = 0; i < 3; i++){
+      if(board[0][i] != ' ' && board[0][i] == board[1][i] && board[0][i] == board [2][i]){
+        printBoard();
+        changeXO();
+        System.out.println("Congradulations! " + xo + " is the winner!\n");
+        return true;
+      }
+      else if(board[i][0] != ' ' && board[i][0] == board[i][1] && board[i][0] == board[i][2]){
+        printBoard();
+        changeXO();
+        System.out.println("Congradulations! " + xo + " is the winner!\n");
+        return true;
+      }
+      else if(board[0][0] != ' ' && board[0][0] == board[1][1] && board[0][0] == board[2][2]){
+        printBoard();
+        changeXO();
+        System.out.println("Congradulations! " + xo + " is the winner!\n");
+        return true;
+      }
+      else if(board[0][2] != ' ' && board[0][2] == board[1][1] && board [0][2] == board[2][0]){
+        printBoard();
+        changeXO();
+        System.out.println("Congradulations! " + xo + " is the winner!\n");
+        return true;
+      }
+      for(int j = 0; j < 3; j++){
+        if(board[i][j] != ' '){
+          counter++;
+          if(counter > 8){
+            printBoard();
+            System.out.println("There are no more moves left, the game is a draw.\n");
+            return true;
+               
+          }
+        }
+      }
+     
+        
+    }
     return false;
     
   }//end of checkWinner
