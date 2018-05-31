9647c4d0ba52a611ccd4e9940ad2cbd8ae45aadb
StevenA83 <36210401+StevenA83@users.noreply.github.com>
2018-05-31 12:48:09 -0700

    Delete SpecimenIronAnklyosaurus.java

--- Java Class/src/com/gamedriver/java/SpecimenIronAnklyosaurus.java
+++
@@ -1,7 +0,0 @@
-package com.gamedriver.java;
-
-public class SpecimenIronAnklyosaurus extends Enemy {
-    public SpecimenIronAnklyosaurus() {
-        super("Specimen 17X: Iron Anklyosaurus", 50, 15, 20);
-    }
-}
